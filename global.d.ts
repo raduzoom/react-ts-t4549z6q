25eda56cb0dd833dcd2ef9d7c5bdd56cf0c1e7f6
raduh <radu.hulubas@gmail.com>
2025-08-25 00:16:00 +0300

    feature: fix instrinsec element

--- global.d.ts
+++ global.d.ts
@@ -1,17 +1,18 @@
 import { DzsChipSelectorWrapper } from 'chip-selector';
 
-declare global {
+declare module 'react' {
   namespace JSX {
     interface IntrinsicElements {
       'dzs-chip-selector': React.DetailedHTMLProps<React.HTMLAttributes<DzsChipSelectorWrapper> & {
         'data-persistentOptions'?: string;
         'data-view-skin'?: string;
         'data-view-is-wrapping'?: string;
-        ref?: React.RefObject<DzsChipSelectorWrapper>;
       }, DzsChipSelectorWrapper>;
     }
   }
+}
 
+declare global {
   interface HTMLElementTagNameMap {
     'dzs-chip-selector': DzsChipSelectorWrapper;
   }
